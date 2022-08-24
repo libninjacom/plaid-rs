5f6a2d52dc396830cc8e5b25d63f832a7a0381ee
Kurt Wolf <kurtwolfbuilds@gmail.com>
2022-08-24 07:38:39 -0400

    fix example

--- examples/item_get.rs
+++ examples/item_get.rs
@@ -6,7 +6,7 @@ async fn main() {
         // Add this middleware to record requests, e.g. for testing.
         // Do not use the middleware in production!
         // .with_middleware(httpclient::middleware::RecorderMiddleware::new());
-    let access_token ="access-sandbox-b4957595-eae2-4130-9da7-114d14726a62".to_string();
+    let access_token ="access-sandbox-b4957595-eae2-4130-9da7-114d14726a62";
     let item_get = client.item_get(access_token)
         .send()
         .await
